c3d1b0bef18f00665acf370101f99402ce83e70b
Shinichirow KAMITO <shinichirow@kamito.net>
2015-03-18 01:16:50 +0900

    Add command `new`.

--- lib/methan/console.rb
+++ lib/methan/console.rb
@@ -2,6 +2,7 @@
 require "thor"
 require "methan/version"
 
+
 module Methan
 
 
@@ -9,14 +10,39 @@ module Methan
 
     desc "version", "show version"
     def version
-      show(Methan::VERSION)
+      show Methan::VERSION
+    end
+
+    desc "new FILENAME", "Create a new memo"
+    option :title, type: :string, desc: %q{Title}, aliases: "t"
+    def new(filename)
+      now = Time.now.strftime("%Y%m%d%H%M%S")
+      pwd = Dir::pwd
+
+      filename = "#{filename}.md" unless filename.split(".").last == "md"
+      filename = "#{now}-#{filename}"
+      filepath = File.join(pwd, filename)
+
+      src = "# #{options[:title] || ''}\n"
+      File.open(filepath, "w") do |io|
+        io.write(src)
+      end
+      show "Create memo `#{filename} at #{pwd}`", :green
     end
 
 
     private
 
-    def show(message)
-      puts message
+    def show(message, color=nil)
+      prefix = case color
+               when :red    then "\033[31m"
+               when :green  then "\033[32m"
+               when :yellow then "\033[33m"
+               when :blue   then "\033[34m"
+               else ""
+               end
+      suffix = prefix == "" ? "" : "\033[0m"
+      puts "#{prefix}#{message}#{suffix}"
     end
   end
 
